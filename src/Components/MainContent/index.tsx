e928b2259cf3d6f9c3d849b0571490806f4cf8c8
Vicente <vicente19981@live.com>
2020-10-06 14:51:31 -0400

    feat: update MainContent

--- src/Components/MainContent/index.tsx
+++ src/Components/MainContent/index.tsx
@@ -1,34 +1,71 @@
-import React from 'react';
+import React from "react";
 
-
-import { Container ,Card,CardsWrapper,ProgressBadge} from './styles';
+import {
+  Container,
+  Card,
+  CardsWrapper,
+  ProgressBadge,
+  ScheduleWrapper,
+  HoursWrapper,
+  Hour,
+  CardDiscpline,
+  PictureWrapper,
+  Picture,
+} from "./styles";
 
 const MainContent: React.FC = () => {
   return (
-      <Container>
-          <CardsWrapper>
-                <Card variants="green">
-                           <div>
-                                   <h4>Your <strong>ratting</strong> <br/> is perfect: </h4>
-                                   <ProgressBadge variants="green">
-                                        <p>81%</p>
-                                   </ProgressBadge>
-                           </div>
-                </Card>
-                <Card variants="white">
-                        <div>
-                                   <h4>Your <strong>ratting</strong> <br/> is perfect: </h4>
-                                   <ProgressBadge variants="white">
-                                        <p>81%</p>
-                                   </ProgressBadge>
-                           </div>
-                </Card>
-          </CardsWrapper>
+    <Container>
+      <CardsWrapper>
+        <Card variants="green">
           <div>
-             
+            <h4>
+              Your <strong>ratting</strong> <br /> is perfect:{" "}
+            </h4>
+            <ProgressBadge variants="green">
+              <p>81%</p>
+            </ProgressBadge>
           </div>
-      </Container>
+        </Card>
+        <Card variants="white">
+          <div>
+            <h4>
+              Your <strong>ratting</strong> <br /> is perfect:{" "}
+            </h4>
+            <ProgressBadge variants="white">
+              <p>81%</p>
+            </ProgressBadge>
+          </div>
+        </Card>
+      </CardsWrapper>
+      <ScheduleWrapper>
+        <h1>Today's schedule </h1>
+        <p>september,4 Friday</p>
+      </ScheduleWrapper>
+      <HoursWrapper>
+        <Hour>
+          <p>10 am</p>
+        </Hour>
+        <Hour>
+          <p>11 am</p>
+        </Hour>
+        <Hour>
+          <p>12 am</p>
+        </Hour>
+        <CardDiscpline>
+          <h1>Mathematics</h1>
+          <p>Properties of number</p>
+          <PictureWrapper>
+            <Picture />
+            <Picture />
+            <Picture />
+            <Picture />
+            <button>Invite</button>
+          </PictureWrapper>
+        </CardDiscpline>
+      </HoursWrapper>
+    </Container>
   );
-}
+};
 
 export default MainContent;
--- src/Components/MainContent/styles.ts
+++ src/Components/MainContent/styles.ts
@@ -1,84 +1,189 @@
-import styled,{css} from 'styled-components';
+import styled, { css } from "styled-components";
 
-
-type ColorVariantProps = { 
-    variants?: "white" | "green";
-}
+type ColorVariantProps = {
+  variants?: "white" | "green";
+};
 
 const colorCardVariants = {
-    green:  css`
-        background-color:#275E2E;
-        color:#FFF;
-    `,
-    white: css`
-        background-color:#FFF;    
-        color:#275E2E;
-    `
-}
+  green: css`
+    background-color: #275e2e;
+    color: #fff;
+  `,
+  white: css`
+    background-color: #fff;
+    color: #275e2e;
+  `,
+};
 
 const colorBadgeVariants = {
-    green:  css`
-        background-color:#638567;
-        color:#FFF;
-`,
-white: css`
-    background-color:#EFEFEF;
-    color:#275E2E;  
-`
-} 
-
+  green: css`
+    background-color: #638567;
+    color: #fff;
+  `,
+  white: css`
+    background-color: #efefef;
+    color: #275e2e;
+  `,
+};
 
 export const Container = styled.div`
-    display:flex;
-    flex-direction:column;
-    justify-content:space-around;
-    background: #F8F8F8;
-    border: 1px solid #EFEFEF;
-    border-radius: 50px 50px 0px 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  background: #f8f8f8;
+  border: 1px solid #efefef;
+  border-radius: 50px 50px 0px 0px;
 `;
 
 export const CardsWrapper = styled.div`
-    display:flex;
-    align-items:center;
-    justify-content:space-around;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
 `;
-
 
 export const ProgressBadge = styled.div<ColorVariantProps>`
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    width: 53px;
-    height: 49px;    
-    border-radius: 10px;
-    ${(props) => colorBadgeVariants[props.variants || "white"]};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 53px;
+  height: 49px;
+  border-radius: 10px;
+  ${(props) => colorBadgeVariants[props.variants || "white"]};
 `;
 
+export const Card = styled.div<ColorVariantProps>`
+  display: flex;
+  align-items: flex-end;
+  justify-content: start;
+  padding: 12px;
+  width: 261px;
+  height: 273px;
+  border-radius: 50px;
+  border: 1px solid #efefef;
+  ${(props) => colorCardVariants[props.variants || "white"]}
 
-export const Card = styled.div<ColorVariantProps>` 
-    display:flex; 
-    align-items:flex-end;
-    justify-content:start;
-    padding:12px;
-    width: 261px;
-    height: 273px;
+  > div {
+    width: 200px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 0 20px 20px;
+  }
+  h4 {
+    line-height: 25px;
+    font-weight: 400;
+  }
+  strong {
+    font-weight: 700;
+  }
+`;
+
+export const ScheduleWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  h1 {
+    font-size: 25px;
+    color: var(--primary-color);
+    font-weight: 500;
+  }
+  p {
+    font-size: 19px;
+    color: var(--color-border);
+  }
+`;
+
+export const HoursWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding: 12px;
+  width: 600px;
+  margin: 0 0 0 auto;
+
+  > div {
+    margin-bottom: 24px;
+  }
+`;
+
+export const Hour = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50px;
+  background: var(--color-border);
+  width: 86px;
+  height: 29px;
+
+  &::after {
+    position: absolute;
+    content: "";
+    width: 500px;
+    height: 1px;
+    background-color: var(--color-border);
+    left: 100%;
+  }
+
+  p {
+    font-weight: 700;
+    font-size: 16px;
+    color: var(--primary-color);
+  }
+`;
+
+export const CardDiscpline = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  width: 300px;
+  height: 150px;
+  padding: 16px;
+
+  h1 {
+    font-weight: 700;
+    font-size: 16px;
+    color: var(--primary-color);
+  }
+
+  p {
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 20px;
+
+    color: var(--text-color);
+  }
+
+  border: 1px solid var(--color-border);
+  background: #ffffff;
+  border-radius: 20px;
+  left: calc(50% - 120px);
+  top: 0;
+`;
+
+export const PictureWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 24px;
+
+  > div {
+    margin-right: 12px;
+  }
+
+  button {
+    background: var(--primary-color);
     border-radius: 50px;
-    border: 1px solid #EFEFEF;
-    ${(props) => colorCardVariants[props.variants || "white"]}
-    
-    > div {
-        width:200px;
-        display:flex;
-        align-items:center;
-        justify-content:space-between;
-        margin: 0 0 20px 20px;
-    }
-    h4{
-            line-height:25px;
-            font-weight:400;
-    }
-    strong{
-        font-weight:700;
-    }
-
+    width: 80px;
+    height: 30px;
+    color: var(--secondary-color);
+  }
+`;
+export const Picture = styled.div`
+  width: 41px;
+  height: 34px;
+  background-color: var(--color-border);
+  border-radius: 50%;
 `;
